f933273f4e7d977f594f5eeff4c6ba772744932e
chickie8x <chickie8x.qn@gmail.com>
2025-01-15 21:58:23 +0700

    minor fix

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,192 @@
 <template>
   <div v-if="isAuth" class="flex flex-col gap-2 p-4 text-sm h-screen">
     <div>
-      <Select v-model="sessionType" :options="typeOptions" class="w-full border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"/>
+      <Select
+        v-model="sessionType"
+        :options="typeOptions"
+        class="w-full border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"
+      />
     </div>
     <div>
-      <input v-model="code" @input="handleAddItem" placeholder="Chạm vào đây để quét" class="w-full border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500" />
+      <input
+        v-model="code"
+        @input="handleAddItem"
+        placeholder="Chạm vào đây để quét"
+        class="w-full border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"
+      />
     </div>
     <div class="flex-grow overflow-y-auto">
       <ListItem :items="listItems" :contentType="sessionType" />
     </div>
     <div class="flex flex-row gap-1">
-      <Select v-model="transport" :options="transportOptions" class="flex-1 border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"/>
-      <Select v-model="goodsStatus" :options="goodsStatusOptions" class="flex-1 border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"/>
+      <Select
+        v-model="transport"
+        :options="transportOptions"
+        class="flex-1 border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"
+      />
+      <Select
+        v-model="goodsStatus"
+        :options="goodsStatusOptions"
+        class="flex-1 border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"
+      />
     </div>
     <div>
-      <textarea v-model="note" placeholder="Nhập ghi chú" class="w-full border border-gray-300 rounded-sm h-10 px-4 py-2 bg-gray-100 outline-indigo-500 resize-none" />
+      <textarea
+        v-model="note"
+        placeholder="Nhập ghi chú"
+        class="w-full border border-gray-300 rounded-sm h-10 px-4 py-2 bg-gray-100 outline-indigo-500 resize-none"
+      />
     </div>
     <div class="flex items-center justify-center gap-2">
-      <button @click="handleReset" class="bg-yellow-500 focus:bg-yellow-400 text-white px-4 py-2 rounded-sm flex-1">Làm lại</button>
-      <button @click="handleCloseSession" class="bg-red-500 focus:bg-red-400 text-white px-4 py-2 rounded-sm flex-1">Đóng phiên</button>
+      <button
+        @click="handleReset"
+        class="bg-yellow-500 focus:bg-yellow-400 text-white px-4 py-2 rounded-sm flex-1"
+      >
+        Làm lại
+      </button>
+      <button
+        @click="handleCloseSession"
+        class="bg-red-500 focus:bg-red-400 text-white px-4 py-2 rounded-sm flex-1"
+      >
+        Đóng phiên
+      </button>
     </div>
   </div>
   <div v-else class="flex items-center justify-center h-screen bg-slate-200 text-sm">
     <div class="flex flex-col gap-2 w-full max-w-md bg-white p-4 rounded-md shadow-md">
       <h1 class="text-2xl font-bold text-center mb-8">Đăng nhập</h1>
-      <div v-if="errorMessage" class="text-red-500 text-center mb-4">{{ errorMessage }}</div>
-      <input v-model="email" type="email" placeholder="Email" class="border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500" />
-      <input v-model="password" type="password" placeholder="Mật khẩu" class="border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500" />
-      <button @click="handleAuth" class="bg-blue-500 focus:bg-blue-400 text-white px-4 py-2 rounded-sm flex-1 my-4">Đăng nhập</button>
+      <div
+        v-if="errorMessage"
+        class="text-red-500 text-center mb-4 text-sm block w-full bg-red-100 p-2 rounded-sm"
+      >
+        &#9888;{{ errorMessage }}
+      </div>
+      <input
+        v-model="email"
+        type="email"
+        placeholder="Email"
+        class="border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"
+      />
+      <input
+        v-model="password"
+        type="password"
+        placeholder="Mật khẩu"
+        class="border border-gray-300 rounded-sm h-10 px-4 bg-gray-100 outline-indigo-500"
+      />
+      <button
+        @click="handleAuth"
+        class="bg-blue-500 focus:bg-blue-400 text-white px-4 py-2 rounded-sm flex-1 my-4"
+      >
+        Đăng nhập
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import axios from "axios";
-import Select from "./components/kits/select/index.vue";
-import ListItem from "./components/kits/list/index.vue";
-import { typeOptions, items, transportOptions, goodsStatusOptions } from "./config";
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import Select from './components/kits/select/index.vue'
+import ListItem from './components/kits/list/index.vue'
+import { typeOptions, items, transportOptions, goodsStatusOptions } from './config'
 
-
-const sessionType = ref(typeOptions[0].value);
-const code = ref("");
-const listItems = ref([]);
-const transport = ref(transportOptions[0].value);
-const goodsStatus = ref(goodsStatusOptions[0].value);
-const note = ref("");
-const timeout = ref(null);
-const isAuth = ref(false);
-const email = ref("");
-const password = ref("");
-const errorMessage = ref("");
+const sessionType = ref(typeOptions[0].value)
+const code = ref('')
+const listItems = ref([])
+const transport = ref(transportOptions[0].value)
+const goodsStatus = ref(goodsStatusOptions[0].value)
+const note = ref('')
+const timeout = ref(null)
+const email = ref('')
+const password = ref('')
+const errorMessage = ref('')
+const token = ref(null);
+const isAuth = ref(token.value ? true : false)
 
 const handleReset = () => {
-  listItems.value = [];
-  code.value = "";
-  transport.value = transportOptions[0].value;
-  goodsStatus.value = goodsStatusOptions[0].value;
-  note.value = "";
-};
+  listItems.value = []
+  code.value = ''
+  transport.value = transportOptions[0].value
+  goodsStatus.value = goodsStatusOptions[0].value
+  note.value = ''
+}
 
 const handleCloseSession = () => {
+  if (!token.value) {
+    console.log("Not authenticated")
+    return
+  }
+  if (listItems.value.length === 0) {
+    return
+  }
+  const sessionCode = `LC_${new Date().getDate()}${new Date().getMonth() + 1}${new Date().getFullYear()}${new Date().getHours()}${new Date().getMinutes()}${new Date().getSeconds()}`
   let data = {
-    listItems: listItems.value,
-    code: code.value,
-    transport: transport.value,
-    goodsStatus: goodsStatus.value,
+    sessionCode: sessionCode,
+    sessionType: sessionType.value,
+    transportCode: listItems.value,
     note: note.value,
-  };
-  console.log(data);
-};
+    goodsStatus: goodsStatus.value,
+    transporter: transport.value,
+    transportCodeQuantity: listItems.value.length,
+  }
+  axios.post("http://localhost:3000/session-transport/create", data, {
+    headers: {
+      authorization: `Bearer ${token.value}`,
+    },
+  })
+  .then((res) => {
+    console.log(res.data)
+    handleReset()
+    alert("Lưu phiên thành công")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
 
 const handleAddItem = () => {
-  clearTimeout(timeout.value);
+  clearTimeout(timeout.value)
   timeout.value = setTimeout(() => {
     if (code.value.trim()) {
-      listItems.value.unshift(code.value);
-      code.value = "";
+      listItems.value.unshift({
+        code: code.value,
+        transporter: transport.value,
+        goodsStatus: goodsStatus.value,
+      })
+      code.value = ''
     }
-  }, 500);
-};
+  }, 500)
+}
 
 const handleAuth = () => {
   if (email.value && password.value) {
-    axios.post("http://localhost:3000/login", {
-      email: email.value,
-      password: password.value,
-    }).then((res) => {
-      if (res.data.token) {
-        isAuth.value = true;
-        console.log(res.data.token);
-      }
-    }).catch((err) => {
-      errorMessage.value = err.response.data.message;
-      console.log(err.response.data.message);
-    });
+    axios
+      .post('http://localhost:3000/login', {
+        email: email.value,
+        password: password.value,
+      })
+      .then((res) => {
+        if (res.data.token) {
+          isAuth.value = true
+          console.log(res.data.token)
+          localStorage.setItem("token", res.data.token);
+        }
+      })
+      .catch((err) => {
+        errorMessage.value = err.response.data.message
+        setTimeout(() => {
+          errorMessage.value = ''
+        }, 4000)
+        password.value = ''
+      })
   }
-};
-</script>
+}
 
+onMounted(() => {
+  token.value = localStorage.getItem("token");
+  if (token.value) {
+    isAuth.value = true
+  }
+})
+
+</script>
--- src/components/kits/list/index.vue
+++ src/components/kits/list/index.vue
@@ -1,26 +1,33 @@
 <template>
-    <div class="flex flex-col border border-gray-300 rounded-sm h-full overflow-y-auto" :class="contentType === 'input' ? 'bg-green-100' : 'bg-yellow-100'">
-        <div v-for="(item, idx) in items" :key="idx" class="flex flex-row border-b border-gray-300 rounded-sm">
-            <div class="w-14 flex justify-center items-center border-r border-gray-300 py-2">
-                <span>{{ idx + 1 }}</span>
-            </div>
-            <div class="flex flex-1 justify-center items-center py-2">
-                <span>{{ item }}</span>
-            </div>
-        </div>
+  <div
+    class="flex flex-col border border-gray-300 rounded-sm h-full overflow-y-auto"
+    :class="contentType === 'IN' ? 'bg-green-100' : 'bg-yellow-100'"
+  >
+    <div
+      v-for="(item, idx) in items"
+      :key="idx"
+      class="flex flex-row border-b border-gray-300 rounded-sm"
+    >
+      <div class="w-14 flex justify-center items-center border-r border-gray-300 py-2">
+        <span>{{ idx + 1 }}</span>
+      </div>
+      <div class="flex flex-1 justify-center items-center py-2">
+        <span>{{ item.code }}</span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
 const props = defineProps({
-    items: {
-        type: Array,
-        required: true,
-    },
-    contentType: {
-        type: String,
-        required: true,
-        default: () => 'input',
-    },
-});
+  items: {
+    type: Array,
+    required: true,
+  },
+  contentType: {
+    type: String,
+    required: true,
+    default: () => 'IN',
+  },
+})
 </script>
